1d93968a16a6430e61a8acef977c7204ce4571f0
eunsung <edplab2019@gmail.com>
2024-09-20 16:52:19 +0900

    책임분리: crawl, preprocess, repository, update db

---
+++ crawl.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+from trafilatura import feeds, fetch_url, extract
+
+
+class Crawler(ABC):
+    @abstractmethod
+    def crawl(self, url):
+        raise NotImplementedError
+
+
+class TrafilaturaCrawler(Crawler):
+
+    def crawl(self, url) -> list[str]:
+        contents = []
+        feed_list = feeds.find_feed_urls(url)
+        
+        for feed in feed_list:
+            html = fetch_url(feed)
+            text = extract(html)
+            contents.append(text)
+    
+        return contents
+
+
